8c64a5c6ba9ad315a5a41005425876ea87694cde
Upendra Kumar <uk06704@gmail.com>
2021-06-09 20:14:56 +0530

    created the prdict function in app.py succesfully

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
 from flask import Flask, render_template, request
-import jsonify
 import numpy as np
+import pandas as pd
 import sklearn
 from joblib import load
 
 model = load("model.pkl")
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-if __name__=="__main__":
+
+@app.route("/predict",methods=["POST", "GET"])
+def predict():
+    if request.method == "POST":
+        model = load("model.pkl")
+        year = int(request.form["year"])
+        present_price = int(request.form["present_price"])
+        km_driven = int(request.form["km_driven"])
+        n_owner = int(request.form["owner"])
+        fuel_type_petrol = fuel_type_diesel = 0
+        if(request.form["fuel_type"] == "Petrol"):
+            fuel_type_petrol = 1
+        elif(request.form["fuel_type"] == "Diesel"):
+            fuel_type_diesel = 1
+        seller_type_indvidual = 0
+        if(request.form["seller_type"] == "Individual"):
+            seller_type_indvidual = 1
+        transmission_mannual = 0
+        if(request.form["gear_type"] == "mannual"):
+            transmission_mannual = 1
+        x = [[year, present_price, km_driven, n_owner, fuel_type_diesel,
+             fuel_type_petrol, seller_type_indvidual, transmission_mannual]]
+        y = model.predict(x)[0]
+        y = format(str(y))
+        return render_template("index.html", price=y)
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
